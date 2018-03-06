56fe6a8292d6d49c267f7cd4fb36adae07a635a9
Venus <Venus>
2018-03-06 22:51:49 +0200

    Save

---
+++ bot.rb
@@ -0,0 +1,5 @@
+require 'discordrb'
+bot = Discordrb::Bot.new token: 'NDE5MjgwMzk0MjUzNTAwNDI2.DYCQuw.-BKvyyb5cVhxP-UTBlmtaqzLt6A'
+bot.message(with_text: "!ping") { |condition| condition.respond "Pong!" }
+
+bot.run
