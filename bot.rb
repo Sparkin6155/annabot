e95b70c8a729c911820d246b07ac31afc10f3863
Venus <Venus>
2018-03-06 22:56:47 +0200

    te

--- bot.rb
+++ bot.rb
@@ -1,4 +1,4 @@
-require 'discordrb'
+require_relative 'Gemfile'
 bot = Discordrb::Bot.new token: 'NDE5MjgwMzk0MjUzNTAwNDI2.DYCQuw.-BKvyyb5cVhxP-UTBlmtaqzLt6A'
 bot.message(with_text: "!ping") { |condition| condition.respond "Pong!" }
 
