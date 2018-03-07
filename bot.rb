26186a878c48861c562f635dd669e096d2a2c138
Venus <Venus>
2018-03-07 21:19:46 +0200

    Ruby Quit-
    Heroku is not accepting Ruby, therefore the best option is to quit using it on developing a discord bot. However, a solution is still being searched.

--- bot.rb
+++
@@ -1,5 +0,0 @@
-require_relative 'Gemfile'
-bot = Discordrb::Bot.new token: 'NDE5MjgwMzk0MjUzNTAwNDI2.DYHMLw.ARI_VsEctaNP8bBFHAofjFSsqxY'
-bot.message(with_text: "!ping") { |condition| condition.respond "Pong!" }
-
-bot.run
